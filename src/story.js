a70223dffcb2f542b76f300daa8752739381e6b8
Andrey Lebedev <lebedancer@gmail.com>
2017-07-24 11:24:07 +0300

    add component

---
+++ src/story.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Button from "./components/Button";
+
+storiesOf('Button', module)
+    .add('default', () => <Button onClick={action('нажал')}>Жми</Button>)
+    .add('big', () => <Button size="big" onClick={action('нажал')}>Жми</Button>)
+    .add('small', () => <Button size="small" onClick={action('нажал')}>Жми</Button>)
+    .add('mini', () => <Button size="mini" onClick={action('нажал')}>Жми</Button>)
+    .add('white', () => <Button onClick={action('нажал')} color='white'>Жми</Button>)
+    .add('loading', () => <Button onClick={action('нажал')} loading={true}>Жми</Button>)
+    .add('loading big', () => <Button size="big" loading={true} onClick={action('нажал')}>Жми</Button>)
+    .add('loading small', () => <Button size="small" loading={true} onClick={action('нажал')}>Жми</Button>)
+    .add('loading mini', () => <Button size="mini" loading={true} onClick={action('нажал')}>Жми</Button>)
+    .add('loading white', () => <Button onClick={action('нажал')} loading={true} color='white'>Жми</Button>)
+    .add('disabled', () => <Button onClick={action('нажал')} disabled={true}>Жми</Button>)
+    .add('disabled white', () => <Button onClick={action('нажал')} disabled={true} color='white'>Жми</Button>)
+    .add('with icon left', () => <Button onClick={action('нажал')}>{svgIconHelper.getJsx({})}Жми</Button>)
+    .add('with icon right', () => <Button onClick={action('нажал')}>Жми{svgIconHelper.getJsx({})}</Button>)
+    .add('with icons big', () => <Button size="big" onClick={action('нажал')}>{svgIconHelper.getJsx({})}Жми{svgIconHelper.getJsx({})}</Button>)
+    .add('with icons small', () => <Button size="small" onClick={action('нажал')}>{svgIconHelper.getJsx({})}Жми{svgIconHelper.getJsx({})}</Button>)
+    .add('with icons mini', () => <Button size="mini" onClick={action('нажал')}>{svgIconHelper.getJsx({})}Жми{svgIconHelper.getJsx({})}</Button>)
+    .add('with icons white', () => <Button color="white" onClick={action('нажал')}>{svgIconHelper.getJsx({})}Жми{svgIconHelper.getJsx({})}</Button>)
+    .add('with icons loading', () => <Button loading={true} onClick={action('нажал')}>{svgIconHelper.getJsx({})}Жми{svgIconHelper.getJsx({})}</Button>)
